801176abc4662788744d8c2f6ec89d544acaf7de
Madison Kerndt <madison.kerndt@gmail.com>
2016-12-14 16:08:01 -0700

    setup native ios app

---
+++ native/app/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export class Main extends Component {
+  state = {
+    horizonalIsOn: false,
+  };
+
+  render() {
+    return(
+      <View />
+    )
+  }
+}
